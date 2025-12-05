ceaced4d0bf569a479464190cc673e48f190d61c
Mathilde GUDIN <mgudin@IFREMER.FR>
2025-12-05 11:13:30 +0100

    script to run species distribution model

--- 00_source.R
+++ 00_source.R
@@ -1,104 +1,137 @@
 
-##################################################################################
+# ------------------------------------------------------------------------------#
+# ------------------------------------------------------------------------------#
+
 # SCRIPT TO RUN SPECIES DISTRIBUTION MODELS
-##################################################################################
+
+# ------------------------------------------------------------------------------#
+# ------------------------------------------------------------------------------#
+
 
 rm(list=ls())
 
-# -------------------------------------------------------------------------------
-# LOAD PACKAGES
-# -------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------#
+####  LOAD PACKAGES  #### 
+# ------------------------------------------------------------------------------#
 
 library(here)
-source(here('03_MODEL/packages/packages.R'))
+source(here('04_MODEL/packages/packages.R'))
 
 
-# ------------------------------------------------------------------------------
-# PARAMETERS
-# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------#
+####  DEFINE PARAMETERS  #### 
+# ------------------------------------------------------------------------------#
 
-# Define species
-sp_scientific = "Solea solea" 
+## Define species
+# ----------------#
+sp_scientific = "Solea solea"
+# sp_scientific = "Dicentrarchus labrax"
 
+common_name = "sole"
+# common_name = "seabass"
 
-# ------------------------------------------------------------------------------
-# LOAD AND TRANSFORM DATA
-# ------------------------------------------------------------------------------
-
-source(here('02_TRANSFORM_DATA/transform_data_CGFS_catch_operation.R'))
-
-
-
-
-
+## Define domain
+# ----------------#
+# study_domain = "Eastern_English_Channel"
+study_domain = "Entire_English_Channel"
 
 
+# ------------------------------------------------------------------------------#
+####  LOAD AND TRANSFORM DATA  #### 
+# ------------------------------------------------------------------------------#
+
+source(here('03_TRANSFORM_DATA/transform_data.R'))
 
 
+# ------------------------------------------------------------------------------#
+####  CREATE THE MESH  #### 
+# ------------------------------------------------------------------------------#
+
+# Cutoff defines the minimum allowed distance between mesh vertices 
+cutoff = 10
+
+source(here('04_MODEL/functions/create_mesh.R'))
 
 
+# ------------------------------------------------------------------------------#
+####  RUN MODEL  #### 
+# ------------------------------------------------------------------------------#
+
+# Select the model version 
+# --------------------------#
+
+# model_version <- "m1_presence_year"
+# model_version <- "m2_density_year"
+# model_version <- "m3_presence_year_depth"
+model_version <- "m4_density_year_depth"
+
+source(here(paste0("04_MODEL/models_versions/models.R")))
 
 
-source(here('03_MODEL/functions/create_domain.R'))
+# ------------------------------------------------------------------------------#
+####  CHECK OUTPUTS  #### 
+# ------------------------------------------------------------------------------#
+
+summary(model)
+
+sanity(model)
+
+# Fixed effects with confidence intervals
+tidy(model)
+
+# Random effects and variance parameters
+tidy(model, "ran_pars")
 
 
+# ------------------------------------------------------------------------------#
+####  DESIGN THE PREDICTION GRID  #### 
+# ------------------------------------------------------------------------------#
 
-# ------------------------------------------------------------------------------
-# PLOT DATA
-# ------------------------------------------------------------------------------
+# grid resolution in km
+res_km = 6
+
+source(here('04_MODEL/functions/design_grid.R'))
+
+
+# ------------------------------------------------------------------------------#
+####  PREDICT ON THE GRID #### 
+# ------------------------------------------------------------------------------#
+
+source(here("04_MODEL/functions/predict_on_grid.R"))
+
 
 # -------------------------------------------------------------------------------
-# MODEL VERSION 
+####  CREATE NEW REPERTORY FOR SAVING OUTPUTS ####  
 # -------------------------------------------------------------------------------
 
-
-# -------------------------------------------------------------------------------
-# CREATE NEW REPERTORY FOR SAVING INPUTS AND OUTPUTS
-# -------------------------------------------------------------------------------
-
-new_repertory_model <- here(paste0("04_outputs/",model_version,"/", format(Sys.time(), "%d-%b-%Y-%H.%M")))
+new_repertory_model <- here(paste0("05_OUTPUTS/", study_domain,"/", common_name,"/", model_version,"/", 
+                                   format(Sys.time(), "%d-%b-%Y-%H.%M")))
 dir.create(new_repertory_model)
 
 
-# -------------------------------------------------------------------------------
-# LOAD THE DATA AUTOMATICALLY BASED ON MODEL VERSION
-# -------------------------------------------------------------------------------
-
-source(here(paste0("02-TRANSFORM-DATA/transform-data_", model_version, ".R")))
-
-# Save the inputs 
-save(Data, file = paste0(new_repertory_model, "/", "inputs_data.RData"))
+rmarkdown::render(input = here("04_MODEL", "report", "report.Rmd"),
+                  output_dir = new_repertory_model, 
+                  output_file = paste0("report_", common_name, "_", model_version, ".html"))
 
 
-# -------------------------------------------------------------------------------
-# PLOT INPUT DATA 
-# -------------------------------------------------------------------------------
 
-# Visualize and check input data : saves plots as a pdf in "new_repertory_model"
-if (!(model_version %in% c("M0_Nourdem", "M0_length"))) {
-  source(here("03_models/r_functions", "plot_input_data_age.R"))
-}
+# ------------------------------------------------------------------------------#
+####  GET INDEX  (abundance, density) #### 
+# ------------------------------------------------------------------------------#
 
-# -------------------------------------------------------------------------------
-# LOAD PARAMETERS
-# -------------------------------------------------------------------------------
-
-source(here('03_models/r_functions/pars_tmb.R'))
-
-# Save the initial parameters 
-save(pars, file = paste0(new_repertory_model, "/", "inputs_parameters.RData"))
-
-
-# -------------------------------------------------------------------------------
-# RUN MODEL
-# -------------------------------------------------------------------------------
-
-source(here('03_models/r_functions/run.R'))
-
-
-# -------------------------------------------------------------------------------
-# PLOT OUTPUTS 
-# ------------------------------------------------------------------------------
-if (!(model_version %in% c("M0_Nourdem", "M0_length"))) {
-  source(here("03_models/r_functions", "plot_outputs_age.R"))
-}
+# DONE ALSO IN REPORT
+# pred_fit_index <- predict(model, newdata = grid_pred,
+#                           return_tmb_object = TRUE)
+# 
+# index <- get_index(pred_fit_index, 
+#                    bias_correct = TRUE, 
+#                    level = 0.95, 
+#                    area = (res_km*res_km),  # area of a projection grid cell
+#                    silent = TRUE)
+# 
+# 
+# ggplot(index, aes(x = factor(year), y = est)) +
+#   geom_point() +
+#   geom_errorbar(aes(ymin = lwr, ymax = upr), alpha = 0.2) +
+#   labs(x = "Year", y = "Density index", title = "Density index") +
+#   theme_minimal()
