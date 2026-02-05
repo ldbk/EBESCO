5b66a8adc20f39b0b9eceaa84111db60bb70ad8a
Mathilde GUDIN <mgudin@IFREMER.FR>
2026-02-05 17:51:41 +0100

    MAJ script for running sdm

--- SDM/00_source.R
+++ SDM/00_source.R
@@ -17,39 +17,39 @@ rm(list=ls())
 library(here)
 source(here('04_MODEL/packages/packages.R'))
 
-region_validity_prop_presence <- readRDS(here("05_OUTPUTS/model_diagnostics/model_diagnostics_Chelidonichthys_cuculus/cross_validation_by_region.rds"))
-
-
 # ------------------------------------------------------------------------------#
 ####  DEFINE PARAMETERS  #### 
 # ------------------------------------------------------------------------------#
 
 ## Define species
 # ----------------#
-# sp_scientific <- "Capros aper"               # boarfish / bogue, poisson sanglier
-# sp_scientific <- "Chelidonichthys cuculus"   # red gurnard / grondin rouge
-# sp_scientific <- "Chelidonichthys lucerna"   # tub gurnard / grondin perlon
-# sp_scientific <- "Conger conger"             # conger / congre
-# sp_scientific <- "Dicentrarchus labrax"      # seabass / bar
-# sp_scientific <- "Engraulis encrasicolus"    # anchovy / anchois
-# sp_scientific <- "Eutrigla gurnardus"        # grey gurnard / grondin gris
-# sp_scientific <- "Merluccius merluccius"     # hake / merlu
-# sp_scientific <- "Micromesistius poutassou"  # blue whiting / merlan bleu
-sp_scientific <- "Solea solea"               # sole / sole
-# sp_scientific <- "Trachurus trachurus"       # horse mackerel / chinchard (commun)
-# sp_scientific <- "Zeus faber"                # John dory / Saint-Pierre
+
+# sp_scientific <- "Capros aper"                 # Boarfish / Sanglier
+# sp_scientific <- "Chelidonichthys cuculus"     # Red gurnard / Grondin rouge
+# sp_scientific <- "Chelidonichthys lucerna"     # Tub gurnard / Grondin perlon
+# sp_scientific <- "Conger conger"               # European conger / Congre d'Europe
+# sp_scientific <- "Dicentrarchus labrax"        # European seabass / Bar européen
+# sp_scientific <- "Engraulis encrasicolus"      # European anchovy / Anchois
+# sp_scientific <- "Eutrigla gurnardus"          # Grey gurnard / Grondin gris
+# sp_scientific <- "Melanogrammus aeglefinus"    # Haddock / Églefin
+# sp_scientific <- "Merluccius merluccius"       # European hake / Merlu européen
+# sp_scientific <- "Micromesistius poutassou"    # Blue whiting / Merlan bleu
+# sp_scientific <- "Pollachius pollachius"       # Pollack / Lieu jaune
+# sp_scientific <- "Squalus acanthias"           # Spiny dogfish / Aiguillat commun
+# sp_scientific <- "Trachurus trachurus"         # Atlantic horse mackerel / Chinchard commun
+# sp_scientific <- "Zeus faber"                  # John Dory / Saint-Pierre
+sp_scientific <- "Clupea harengus"             # Atlantic herring / Hareng de l’Atlantique
+# sp_scientific <- "Solea solea"                 # Common sole / Sole commune
+
 
 ## Define domain
 # ----------------#
 
-region_validity_prop_presence <- readRDS(here("01_DATA/region_validity_prop_presence.rds")) %>%
+region_validity_prop_presence <- readRDS(here("01_DATA/species_region_validity.rds"))%>%
   filter(scientificName == sp_scientific)
 
-East_English_Channel = isTRUE(region_validity_prop_presence$east[1])
 West_English_Channel = isTRUE(region_validity_prop_presence$west[1])
-
-# East_English_Channel = TRUE
-# West_English_Channel = TRUE 
+East_English_Channel = isTRUE(region_validity_prop_presence$east[1])
 
 # ------------------------------------------------------------------------------#
 ####  LOAD AND TRANSFORM DATA  #### 
@@ -57,6 +57,18 @@ West_English_Channel = isTRUE(region_validity_prop_presence$west[1])
 
 source(here('03_TRANSFORM_DATA/transform_data.R'))
 
+
+# ------------------------------------------------------------------------------#
+####  COMPUTE AND PLOT MORAN's INDEX = SPATIAL AUTOCORRELATION  #### 
+# ------------------------------------------------------------------------------#
+
+source(here('04_MODEL/functions/compute_plot_Moran.R'))
+
+# ------------------------------------------------------------------------------#
+####  PLOT RESIDUALS OF MODEL FITTED WITHOUT RANDOM FIELD #### 
+# ------------------------------------------------------------------------------#
+
+source(here('04_MODEL/functions/compute_plot_residuals_withoutRF.R'))
 
 # ------------------------------------------------------------------------------#
 ####  CREATE THE MESH  #### 
@@ -82,13 +94,17 @@ families_by_response <- list(
     tweedie = sdmTMB::tweedie(link = "log"),
     deltagamma = delta_gamma(link1 = "logit", link2 = "log"),
     deltalognormal = delta_lognormal(link1 = "logit", link2 = "log"),
-    deltagammapoisson = delta_gamma(type = "poisson-link")
+    deltagammapoissonlink = delta_gamma(type = "poisson-link")#, 
+    # lognormal = lognormal(link = "log"),
+    # gamma = Gamma(link = "log")
   ),
   # Candidate distributions for biomass density
   densityKgKm2 = list(
     tweedie = sdmTMB::tweedie(link = "log"),
     deltagamma = delta_gamma(link1 = "logit", link2 = "log"),
-    deltalognormal = delta_lognormal(link1 = "logit", link2 = "log")
+    deltalognormal = delta_lognormal(link1 = "logit", link2 = "log")#,
+    # lognormal = lognormal(link = "log"),
+    # gamma = Gamma(link = "log")
   )
 )
 
@@ -96,7 +112,7 @@ source(here("04_MODEL/functions/fit_candidate_models.R"))
 
 source(here("04_MODEL/functions/sanity_filter_models.R"))
 
-# View(sanity_by_region)
+source(here("04_MODEL/functions/extract_sdmTMB_params.R"))
 
 
 # ------------------------------------------------------------------------------#
@@ -110,7 +126,7 @@ source(here("04_MODEL/functions/analytical_randomized_quantile_residuals.R"))
 ####  SIMULATION BASED ANALYTICAL RANDOMIZED QUANTILE RESIDUALS  #### 
 # ------------------------------------------------------------------------------#
 
-# source(here("04_MODEL/functions/simulation_based_randomized_quantile_residuals.R"))
+source(here("04_MODEL/functions/simulation_based_randomized_quantile_residuals.R"))
 
 
 # ------------------------------------------------------------------------------#
@@ -122,10 +138,17 @@ source(here("04_MODEL/functions/AIC_weights.R"))
 
 
 # ------------------------------------------------------------------------------#
-####  CROSS-VALIDATION #### 
+####  RANDOM CROSS-VALIDATION #### 
 # ------------------------------------------------------------------------------#
 
-source(here("04_MODEL/functions/cross_validation.R"))
+source(here("04_MODEL/functions/cross_validation_random.R"))
+
+
+# ------------------------------------------------------------------------------#
+####  SPATIO TEMPORAL BLOCKED CROSS-VALIDATION #### 
+# ------------------------------------------------------------------------------#
+
+source(here("04_MODEL/functions/cross_validation_spatiotemporal_block.R"))
 
 # ------------------------------------------------------------------------------#
 ####  SAVE OUTPUTS ####  
@@ -147,9 +170,10 @@ save_model_diagnostics(
   sp_scientific = sp_scientific,
   sanity_by_region = sanity_by_region,
   residuals_by_region = residuals_by_region,
-  # dharma_by_region = dharma_by_region,
+  dharma_by_region = dharma_by_region,
   AIC_by_region = AIC_by_region,
-  cross_validation_by_region = cross_validation_by_region
+  randomCV_by_region = randomCV_by_region,
+  blockedCV_by_region = blockedCV_by_region
 )
 
 # ------------------------------------------------------------------------------#
@@ -157,7 +181,7 @@ save_model_diagnostics(
 # ------------------------------------------------------------------------------#
 
 # grid resolution in km
-res_km = 6
+res_km = 5
 
 source(here('04_MODEL/functions/design_grid.R'))
 
@@ -181,4 +205,5 @@ rmarkdown::render(
     "report_", sp_safe, "_", format(Sys.time(), "%d-%b-%Y-%H.%M"), ".html"
   )
 )
+
 
