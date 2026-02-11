ff30ab6041054cc6d774040e29dd0f814f1c023d
Mathilde GUDIN <mgudin@IFREMER.FR>
2026-02-11 19:04:10 +0100

    simulate and plot distributions for candidate families

---
+++ SDM/04_MODEL/functions/simulate_plot_distribution.R
@@ -0,0 +1,189 @@
+# ==============================================================================#
+# Simulate distributions for candidate families 
+# ==============================================================================#
+
+
+sim_plot_distrib <- function(data_CGFS, region, response){
+
+  
+set.seed(123)
+
+pos_response <- response[response > 0]
+nb_hauls <- length(response)
+
+# Specific case : probability of presence = 1 
+if (sp_scientific == "Trachurus trachurus" && region == "west") {
+  
+  # 1) LOGNORMAL
+  # lognormal parameters
+  sdlog =  sqrt(log(1 + (sd(pos_response)^2)/(mean(pos_response)^2)))
+  meanlog = log(mean(pos_response)) - (1/2)*sdlog^2 
+  
+  # simulate delta-lognormal
+  sim_lognormal <- rlnorm(nb_hauls, meanlog = meanlog, sdlog = sdlog)
+  
+  # 2) GAMMA
+  # gamma parameters
+  gshape = mean(pos_response)^2/sd(pos_response)^2   
+  gscale = sd(pos_response)^2/mean(pos_response)
+  
+  # simulate delta-gamma
+  sim_gamma <- rgamma(nb_hauls, shape = gshape, scale = gscale)
+  
+  # PLOT
+  response_name <- deparse(substitute(response))
+  xlab <- paste(response_name)
+  
+  plot(density(response),
+       lwd = 2,
+       col = "black",
+       xlab = xlab,
+       ylab = "Density",
+       main = "")
+  
+  lines(density(sim_lognormal), lwd = 2, lty = 2, col = "dodgerblue3")
+  lines(density(sim_gamma),  lwd = 2, lty = 2, col = "springgreen4")
+
+  legend("topright",
+         legend = c("Observed", "Lognormal", "Gamma"),
+         col = c("black", "dodgerblue3", "springgreen4"),
+         lwd = 2,
+         lty = c(1, 2, 2),
+         bty = "n")
+  
+  return(recordPlot())
+  
+} else {
+  
+  # ----------------------------------------------------------------------------- #
+  # 1)  Binomial
+  # ----------------------------------------------------------------------------- #
+  
+  # presence probability 
+  classic_ppresence <- mean(response > 0)
+  
+  # simulate binomial
+  sim_binomial <- rbinom(n = nb_hauls,                 # number of observations
+                         size = 1,                     # number of trials 
+                         prob = classic_ppresence)     # probability of success on each trial
+  
+  
+  
+  
+  
+  # ----------------------------------------------------------------------------- #
+  # 2) Delta-lognormal
+  # ----------------------------------------------------------------------------- #
+  
+  # lognormal parameters
+  sdlog =  sqrt(log(1 + (sd(pos_response)^2)/(mean(pos_response)^2)))
+  meanlog = log(mean(pos_response)) - (1/2)*sdlog^2 
+  
+  # simulate delta-lognormal
+  sim_dlognormal <- sim_binomial * rlnorm(nb_hauls, meanlog = meanlog, sdlog = sdlog)
+  
+  
+  
+  # ----------------------------------------------------------------------------- #
+  # 3) Delta-Gamma
+  # ----------------------------------------------------------------------------- #
+  
+  # gamma parameters
+  gshape = mean(pos_response)^2/sd(pos_response)^2   
+  gscale = sd(pos_response)^2/mean(pos_response)
+  
+  # simulate delta-gamma
+  sim_dgamma <- sim_binomial * rgamma(nb_hauls, shape = gshape, scale = gscale)
+  
+  
+  
+  
+  # ----------------------------------------------------------------------------- #
+  # 3) Tweedie
+  # ----------------------------------------------------------------------------- #
+  
+  # Tweedie parameters
+  # # Estimate the Tweedie power parameter by profile likelihood
+  tw_prof1 <- suppressWarnings(tweedie.profile(response ~ 1, p.vec = seq(1.05, 1.95, 0.05)))
+  power <- tw_prof1$p.max
+  # # Estimate phi = dispersion parameter at fixed Tweedie power parameter 
+  tw_prof2 <- suppressWarnings(tweedie.profile(response ~ 1, p.vec = power))
+  phi <- tw_prof2$phi.max
+  mu <- mean(response)
+  
+  # simulate tweedie
+  sim_tweedie <- fishMod::rTweedie(n = nb_hauls, p = power, mu = mu, phi = phi)
+  
+  
+  
+  # ----------------------------------------------------------------------------- #
+  # Plot 
+  # ----------------------------------------------------------------------------- #
+  
+  response_name <- deparse(substitute(response))
+  xlab <- paste(response_name)
+  
+  plot(density(response),
+       lwd = 2,
+       col = "black",
+       xlab = xlab,
+       ylab = "Density",
+       main = "")
+  
+  lines(density(sim_dlognormal), lwd = 2, lty = 2, col = "dodgerblue3")
+  lines(density(sim_dgamma),  lwd = 2, lty = 2, col = "springgreen4")
+  lines(density(sim_tweedie), lty = 2, lwd = 2, col = "darkorange3")
+  
+  legend("topright",
+         legend = c("Observed", "Delta-lognormal", "Delta-gamma", "Tweedie"),
+         col = c("black", "dodgerblue3", "springgreen4", "darkorange3"),
+         lwd = 2,
+         lty = c(1, 2, 2, 2),
+         bty = "n")
+  
+  return(recordPlot())
+  
+}
+
+}
+
+# if (isTRUE(West_English_Channel)) {
+# plot_dist_west_biomass <- sim_plot_distrib(data_CGFS_west, "west", data_CGFS_west$totalWeightKg)
+# plot_dist_west_density <- sim_plot_distrib(data_CGFS_west, "west", data_CGFS_west$densityKgKm2)
+# }
+# if (isTRUE(East_English_Channel)) {
+# plot_dist_east_biomass <- sim_plot_distrib(data_CGFS_east, "east", data_CGFS_east$totalWeightKg)
+# plot_dist_east_density <- sim_plot_distrib(data_CGFS_east, "east", data_CGFS_east$densityKgKm2)
+# }
+
+
+# par(mfrow = c(1, 1))
+# 
+# hist(response,
+#      breaks = "FD",
+#      freq = FALSE,
+#      col = "grey85",
+#      border = "white",
+#      xlab = "Biomass (kg)")
+# 
+# lines(density(response), lwd = 2, col = "black")
+# lines(density(sim_biomass_dln), lwd = 2, col = "green")
+# lines(density(sim_biomass_dg), lwd = 2, col = "red")
+# lines(density(sim_biomass_tweedie), lwd = 2, col = "blue")
+# 
+# legend("topright",
+#        legend = c("Observed",
+#                   "Delta-gamma",
+#                   "Delta-lognormal",
+#                   "Tweedie"),
+#        col = c("black",  "red", "green", "blue"),
+#        lwd = 2,
+#        bty = "n")
+
+
+
+
+
+
+
+
